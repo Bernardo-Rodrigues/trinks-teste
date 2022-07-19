a648b4b3053441a44884064159dec40d2bc72624
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2022-07-19 15:58:43 -0300

    refactor: fix function name

--- index.test.ts
+++ index.test.ts
@@ -1,32 +1,32 @@
 import NumerosFelizesESortudos from ".";
 
 describe("#Index - test suit for index file", () => {
-  it("NumerosFelizesESortudps - should return 'Número Sortudo e Feliz.' given param 7", () => {
+  it("NumerosFelizesESortudos - should return 'Número Sortudo e Feliz.' given param 7", () => {
     const result = NumerosFelizesESortudos(7);
     expect(result).toBe("7 - Número Sortudo e Feliz.");
   });
 
-  it("NumerosFelizesESortudps - should return 'Número Sortudo e Não-Feliz.' given param 21", () => {
+  it("NumerosFelizesESortudos - should return 'Número Sortudo e Não-Feliz.' given param 21", () => {
     const result = NumerosFelizesESortudos(21);
     expect(result).toBe("21 - Número Sortudo e Não-Feliz.");
   });
 
-  it("NumerosFelizesESortudps - should return 'Número Não-Sortudo e Feliz.' given param 28", () => {
+  it("NumerosFelizesESortudos - should return 'Número Não-Sortudo e Feliz.' given param 28", () => {
     const result = NumerosFelizesESortudos(28);
     expect(result).toBe("28 - Número Não-Sortudo e Feliz.");
   });
 
-  it("NumerosFelizesESortudps - should return 'Número Não-Sortudo e Não-Feliz.' given param 142", () => {
+  it("NumerosFelizesESortudos - should return 'Número Não-Sortudo e Não-Feliz.' given param 142", () => {
     const result = NumerosFelizesESortudos(142);
     expect(result).toBe("142 - Número Não-Sortudo e Não-Feliz.");
   });
 
-  it("NumerosFelizesESortudps - should return 'Número Sortudo e Não-Feliz' given param 37", () => {
+  it("NumerosFelizesESortudos - should return 'Número Sortudo e Não-Feliz' given param 37", () => {
     const result = NumerosFelizesESortudos(37);
     expect(result).toBe("37 - Número Sortudo e Não-Feliz.");
   });
 
-  it("NumerosFelizesESortudps - should return 'Número Não-Sortudo e Feliz.' given param 100", () => {
+  it("NumerosFelizesESortudos - should return 'Número Não-Sortudo e Feliz.' given param 100", () => {
     const result = NumerosFelizesESortudos(100);
     expect(result).toBe("100 - Número Não-Sortudo e Feliz.");
   });
